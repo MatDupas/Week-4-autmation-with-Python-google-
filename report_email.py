c3ac487943856d6eeff185f0cfe3160bbd1abac4
MatDupas <mathieu.dupas@innovationpartagee.com>
2020-10-27 10:29:12 +0000

    typos corrected in reports and reports_email

--- report_email.py
+++ report_email.py
@@ -2,10 +2,12 @@
 
 import email.message
 import mimetypes
-import os.path
+import os
 import smtplib
+import reports
+import datetime
 
-def generate(sender, recipient, subject, body, attachment_path):
+def generate_email(sender, recipient, subject, body, attachment_path):
   """Creates an email with an attachement."""
   # Basic Email formatting
   message = email.message.EmailMessage()
@@ -32,3 +34,26 @@ def send(message):
   mail_server = smtplib.SMTP('localhost')
   mail_server.send_message(message)
   mail_server.quit()
+
+
+
+def generate_paragraph(path):
+  paragraph =""
+  for file in os.listdir(path):
+    with open(os.path.join(path,file),'r') as f:
+      text = f.readlines()
+      title, weight, desc = text[0],text[1],text[2]
+      paragraph += '\n'.join("name:"+title,"weight:"+weight,"\n")
+  return paragraph 
+
+
+if __name__ == "__main__":
+    paragraph = generate_paragraph('~/home/student-03-a899d807370d/supplier-data/descriptions')
+    today = datetime.datetime.now().strftime("%B %d,%Y")
+    
+    reports.generate_report('/tmp/processed.pdf', 'Processed Update on '+today, paragraph)
+    
+    body ="All fruits are uploaded to our website successfully. A detailed list is attached to this email."
+    message= generate_email('automation@example', 'student-03-a899d807370d@35.223.118.227',
+                             'Upload Completed - Online Fruit Store', body, '/tmp/processed.pdf'):
+    send(message)
